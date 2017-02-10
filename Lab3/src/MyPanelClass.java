fc534a7658d7494f9b9ccf68d7dda76cf2c270f2
Enrique <Enrique@EGGA>
2017-02-09 20:14:57 -0400

    Centered the oval

--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -6,11 +6,11 @@ import javax.swing.JPanel;
  
 public class MyPanelClass extends JPanel {
             /**
-	 * 
-	 */
-	private static final long serialVersionUID = 7057541440811488699L;
+     * 
+     */
+    private static final long serialVersionUID = 7057541440811488699L;
 
-			public void paintComponent(Graphics g) {
+            public void paintComponent(Graphics g) {
                         super.paintComponent(g);
  
                         //Compute interior coordinates
@@ -25,11 +25,19 @@ public class MyPanelClass extends JPanel {
                         //Paint the background
                         g.setColor(Color.GREEN);
                         g.fillRect(x1, y1, width + 1, height + 1);
-                        //Draw a border
-                        g.setColor(Color.YELLOW);
-                        g.drawRect(x1, y1, width , height);
-                        //Draw a second border
-                        g.setColor(Color.RED);
-                        g.drawRect(x1 + 5, y1 + 5, width - 10, height -10);
+//                        //Draw a border
+//                        g.setColor(Color.YELLOW);
+//                        g.drawRect(x1, y1, width , height);
+//                        //Draw a second border
+//                        g.setColor(Color.RED);
+//                        g.drawRect(x1 + 5, y1 + 5, width - 10, height -10);
+//                        
+//                        g.setColor(Color.WHITE);
+//                        g.drawLine(x1, y1, x2, y2);
+//                        
+//                        g.setColor(Color.BLACK);
+//                        g.drawLine(x2, y1, x1, y2);
+                        g.setColor(Color.LIGHT_GRAY);
+                        g.fillOval(x1 + (width-55)/2, y1 + (height-55)/2, 55, 55);
             }
 }
